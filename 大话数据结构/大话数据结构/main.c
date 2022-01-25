dff38c14dc95c14cc6922f6a0b5f20de87f98936
fengwy <fhero@qq.com>
2022-01-25 10:53:11 +0800

    创建笔记

---
+++ 大话数据结构/大话数据结构/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  大话数据结构
+//
+//  Created by 冯午阳 on 2022/1/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
